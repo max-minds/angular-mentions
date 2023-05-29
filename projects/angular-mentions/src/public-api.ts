1be409785d13d028c5db38011f5bf333a6a42062
Szymon Szeląg <szelagszymon@gmail.com>
2023-05-29 21:35:26 +0200

    Expose utils and mention-list component

--- projects/angular-mentions/src/public-api.ts
+++ projects/angular-mentions/src/public-api.ts
@@ -5,3 +5,6 @@
 export * from './lib/mention-config';
 export * from './lib/mention.directive';
 export * from './lib/mention.module';
+
+export * from './lib/mention-list.component';
+export * from './lib/mention-utils';
